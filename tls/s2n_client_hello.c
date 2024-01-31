a0df70252df0f44a738cd944e639e88dc170e581
Appelmans <madelea@amazon.com>
2024-01-31 14:51:46 -0800

    clang format

--- tls/s2n_client_hello.c
+++ tls/s2n_client_hello.c
@@ -1021,7 +1021,6 @@ int s2n_client_hello_get_server_name_length(struct s2n_client_hello *ch, uint16_
     POSIX_GUARD(s2n_stuffer_read_uint16(&extension_stuffer, length));
 
     return S2N_SUCCESS;
-
 }
 
 int s2n_client_hello_get_server_name(struct s2n_client_hello *ch, uint8_t *buffer, uint16_t length, uint16_t *out_length)
@@ -1030,7 +1029,7 @@ int s2n_client_hello_get_server_name(struct s2n_client_hello *ch, uint8_t *buffe
     *out_length = 0;
     POSIX_ENSURE_REF(ch);
     POSIX_ENSURE_REF(buffer);
-    
+
     s2n_parsed_extension *server_name_extension = NULL;
     POSIX_GUARD(s2n_client_hello_get_parsed_extension(S2N_EXTENSION_SERVER_NAME, &ch->extensions, &server_name_extension));
     POSIX_ENSURE_REF(server_name_extension);
